d0565e6e7655cdde09a8fddd2eb4e0f55bbca449
Haoz03 <haozhong03@gmail.com>
2019-09-18 10:15:25 +0800

    添加文件头部

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+/*
+ * @Author: zhonghao 
+ * @Date: 2019-09-18 10:09:59 
+ * @Last Modified by:   zhonghao 
+ * @Last Modified time: 2019-09-18 10:09:59 
+ */
 <template>
   <div id="app">
     <router-view />
